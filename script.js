13a7931ce910bd4551eb02169fb5878df481933b
green ninja <greeninjawillbewebprogrammer@gmail.com>
2023-01-18 22:39:46 +0500

    he

---
+++ script.js
@@ -0,0 +1,31 @@
+var name = prompt('Ваше имя')
+alert(name)
+
+var old = +prompt('Ваш возраст')
+alert(old)
+
+var plus = +prompt('12 + 6')
+alert(plus)
+
+var minus = +prompt('12 - 6')
+alert(minus)
+
+var multiplication = +prompt('12 * 6')
+alert(multiplication)
+
+var division = +prompt('12 / 6')
+alert(division)
+
+var percent = +prompt('12 % 6')
+alert(percent)
+
+var open = ('Откройте консоль для получения резултатов')
+alert(open)
+
+console.log(' Меня зовут ' + ( name ));
+console.log(' Мне ' + ( old ));
+console.log(' Пример 1: 12 + 6 = 18 Ваш ответ ' + ( plus ));
+console.log(' Пример 2: 12 - 6 = 6 Ваш ответ ' + ( minus ));
+console.log(' Пример 3: 12 * 6 = 72 Ваш ответ ' + ( multiplication ));
+console.log(' Пример 4: 12 / 6 = 2 Ваш ответ ' + ( division ));
+console.log(' Пример 5: 12 % 6 = 0 Ваш ответ ' + ( percent ));
